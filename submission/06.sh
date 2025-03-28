126dfdf00ae4ede8fa4d9843bedcb8b4b0631ad0
Kelvin Isievwore <kelvinator4leo@gmail.com>
2025-03-28 19:20:23 +0100

    update

---
+++ submission/06.sh
@@ -0,0 +1,2 @@
+# What is the hash of this partially signed transaction?
+# transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
---
+++ submission/07.sh
@@ -0,0 +1,2 @@
+# What is the receiver's address in this partially signed transaction?
+# transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
