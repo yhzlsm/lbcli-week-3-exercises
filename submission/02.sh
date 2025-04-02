b984e38cfde7f93858f1058c3408e3b03a3f05ba
yhzlsm <yhzlsm@gmail.com>
2025-04-01 21:45:23 -0300

    feat: add 1 and 2 exercise

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,4 @@
 # Create a native segwit address and get the public key from the address.
+SEGWIT_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
+PUB_KEY=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo "$SEGWIT_ADDRESS" | jq -r '.pubkey')
+echo "$PUB_KEY"
