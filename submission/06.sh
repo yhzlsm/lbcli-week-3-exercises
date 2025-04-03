af616ba82bdf490150f778ff6d796ee913fe1f06
yhzlsm <yhzlsm@gmail.com>
2025-04-03 06:40:11 -0300

    add exercises 3 - 7

--- submission/06.sh
+++ submission/06.sh
@@ -1,2 +1,7 @@
 # What is the hash of this partially signed transaction?
-# transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+PSBT_HASH=$(bitcoin-cli -regtest -named decodepsbt psbt=$transaction | jq -r '.tx.hash' )
+
+echo "$PSBT_HASH"
