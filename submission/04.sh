0dd309af5812481cefaf0be7c60f4b666344b1bb
yhzlsm <yhzlsm@gmail.com>
2025-04-03 06:44:22 -0300

    fix: extract address from multisig

--- submission/04.sh
+++ submission/04.sh
@@ -4,5 +4,6 @@ PUB_KEY_1=024bc198a88b69316952414af34105613c3617561a433a229100a0966cd631dd3d
 PUB_KEY_2=035e11024a7fa1e0ca537e0e76b15c1631486f6611c7d8187aea8f6c1ae5d30617
 PUB_KEY_3=03ce11c7b563c3d7fae916351f234eac5089ad2e42fa9e29a94ee944e00c880c66
 
-MULTISIG_ADDRESS=$(bitcoin-cli -named -regtest createmultisig nrequired=2 keys="[\"$PUB_KEY_1\",\"$PUB_KEY_2\",\"$PUB_KEY_3\"]")
+MULTISIG_ADDRESS=$(bitcoin-cli -named -regtest createmultisig nrequired=2 keys="[\"$PUB_KEY_1\",\"$PUB_KEY_2\",\"$PUB_KEY_3\"]" | jq -r '.address')
+
 echo "$MULTISIG_ADDRESS"
